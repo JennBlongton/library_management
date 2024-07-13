d9cedda73d2701dc918dd423e18f592b907510a6
JennBlongton <sishia14@gmail.com>
2024-07-13 16:09:43 +0530

    Defining Resources for API endpoints

---
+++ resources/author_resource.py
@@ -0,0 +1,42 @@
+from flask_restful import Resource, reqparse
+from services.author_service import AuthorService
+
+
+class AuthorResource(Resource):
+
+    def __init__(self):
+        self.parser = reqparse.RequestParser()
+        self.parser.add_argument('name', type=str, required=True, help='Name is required')
+
+    
+    def get(self, author_id=None):
+        if author_id:
+            author = AuthorService.get_author_by_id(author_id)
+            if author:
+                return {'id': author.id, 'name': author.name}, 200
+            return {'message': 'Author not found'}, 404
+        authors = AuthorService.get_all_authors()
+        return [{'id': author.id, 'name': author.name} for author in authors], 200
+    
+
+    def post(self):
+        args = self.parser.parse_args()
+        author = AuthorService.create_author(args)
+        return {'id': author.id, 'name': author.name}, 201
+    
+
+    def put(self, author_id):
+        args = self.parser.parse_args()
+        author = AuthorService.get_author_by_id(author_id)
+        if author:
+            updated_author = AuthorService.create_author(author, args)
+            return {'id': updated_author.id, 'name': updated_author.name}, 200
+        return {'message': 'Author not found'}, 404
+    
+
+    def delete(self, author_id):
+        author = AuthorService.get_author_by_id(author_id)
+        if author:
+            AuthorService.delete_author(author)
+            return {'message': 'Author Deleted'}, 200
+        return {'message': 'Author not found'}, 404
---
+++ resources/book_resource.py
@@ -0,0 +1,45 @@
+from flask_restful import Resource, reqparse
+from services.book_service import BookService
+
+
+class BookResource(Resource):
+
+    def __init__(self):
+        self.parser = reqparse.RequestParser()
+        self.parser.add_argument('title', type=str, required=True, help='Title is required')
+        self.parser.add_argument('author_id', type=int, required=True, help='Author ID is required')
+        self.parser.add_argument('user_id', type=str, required=False, help='User ID is optional')
+
+
+    def get(self, book_id=None):
+        if book_id:
+            book = BookService.get_book_by_id(book_id)
+            if book:
+                return {'id': book.id, 'title': book.title, 'author_id': book.author_id, 'user_id': book.user_id}, 200
+            return {'message': 'Book not found'}, 404
+        
+        books = BookService.get_all_books()
+        return [{'id': book.id, 'title': book.title, 'author_id': book.author_id, 'user_id': book.user_id} for book in books], 200
+    
+
+    def post(self, book_id):
+        args = self.parser.parse_args()
+        book = BookService.create_book(args)
+        return {'id': book.id, 'title': book.title, 'author_id': book.author_id, 'user_id': book.user_id}, 201
+    
+
+    def put(self, book_id):
+        args = self.parser.parse_args()
+        book = BookService.get_book_by_id(book_id)
+        if book:
+            updated_book = BookService.update_book(book, args)
+            return {'id': updated_book.id, 'title': updated_book.title, 'author_id': updated_book.author_id, 'user_id': updated_book.user_id}, 200
+        return {'message': 'Book not found'}, 404
+    
+
+    def delete(self, book_id):
+        book = BookService.get_book_by_id(book_id)
+        if book:
+            BookService.delete_book(book)
+            return {'message': 'Book deleted'}, 200
+        return {'message': 'Book not found'}, 404
